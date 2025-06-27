12cd583ddd56c0d7ec17e5909a9bf0d78c3863e1
Plycedes <abhaypratapyadav340@gmail.com>
2025-06-27 17:46:46 +0530

    Testing Route
    New route for testing camera device switching logic

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import Test from "./Test.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DeviceAndBrowserGate from "./components/DeviceAndBrowserGate.tsx";
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
                 <Routes>
                     <Route path="/" element={<App />} />
                     <Route path="/:id" element={<App />} />
+                    <Route path="/testing" element={<Test />} />
                 </Routes>
             </BrowserRouter>
         </DeviceAndBrowserGate>
@@ -19,6 +21,7 @@ createRoot(document.getElementById("root")!).render(
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/:id" element={<App />} />
+                <Route path="/testing" element={<Test />} />
             </Routes>
         </BrowserRouter> */}
     </StrictMode>
